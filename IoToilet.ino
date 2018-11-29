3bc3b4c50fe8d0431b83ec55dce921b25a9a8359
xyzarivera <xyzarivera@gmail.com>
2018-11-29 13:26:10 +0800

    updated IFTTT Slack integration

--- IoToilet.ino
+++ IoToilet.ino
@@ -5,11 +5,16 @@
 const char* ssid     = "wifi SSID";
 const char* password = "wifi password";
 
-// IFTTT setup
+// IFTTT setup for Slack
 const char *host = "maker.ifttt.com";
-const char *Maker_Event = "Maker_Event";
-String Your_Key = "Your_Key";
 String CRloc = "Your Location";
+String Maker_Event = "Maker_Event";         //Maker Event for Slack
+String Your_Key = "Your_Key";               //Webhook key for Slack
+
+// If you want to add another Maker event and Key:
+// IFTTT setup for Twitter
+String Maker_Event2 = "Maker_Event2";  //Maker Event for Twitter
+String Your_Key2 = "Your_Key2";  //Webhook key for Twitter
 
 // Assign output variables to GPIO pins
 const int LDRpin = 14;
@@ -18,7 +23,7 @@ const int LDRpin = 14;
 int LDRRead;
 int LDRState = 1;
 int lastLDRState = 1;
-String  printState = "unoccupied";
+String  printState = "OCCUPIED";
 
 // the following variables are unsigned longs because the time, measured in
 // milliseconds, will quickly become a bigger number than can be stored in an int.
@@ -28,7 +33,6 @@ unsigned long debounceDelay = 50;    // the debounce time; increase if the outpu
 void setup() {
   // Initialize the output variables as outputs
   pinMode(LDRpin, INPUT);
-
   Serial.begin(115200);
   delay(3000);
   wifiConnect();
@@ -56,7 +60,8 @@ void loop() {
     if (LDRRead != LDRState) {
       Serial.println("if LDRState");
       LDRState = LDRRead;
-      sendTweet(LDRState);
+      slackMessage(LDRState);     // Send http request to Post on Slack
+      sendTweet(LDRState);        // Send http request to Post on Twitter
     }
   }
   // save the reading. Next time through the loop, it'll be the lastButtonState:
@@ -83,36 +88,76 @@ void wifiConnect() {
   Serial.println(WiFi.localIP());
 }
 
-void sendTweet(int State) {
+void slackMessage(int State) {
   if (State == HIGH) {
     // state = high = no light;
-    printState = "unoccupied";
+    printState = "VACANT";
+    Serial.println(printState);     //for debugging
   }
   else  {
     //  state = low = light present;
-    printState = "occupied";
+    printState = "OCCUPIED";
+    Serial.println(printState);     //for debugging    
   }
-
+  
   // Use WiFiClient class to create TCP connections
   WiFiClient client;
   const int httpPort = 80;
-
   while (!client.connect(host, httpPort)) {
     delay(500);
     Serial.print(".");
   }
-
   // create URL request
-  // https://maker.ifttt.com/trigger/IoToilet_tweet/with/key/Your_Key/?value1=LG&value2=unoccupied
-  String url = "/trigger/"+ Maker_Event +"/with/key/" +Your_Key + "/?value1=" + CRloc + "&value2=" + printState;
+  // https://maker.ifttt.com/trigger/IoToilet/with/key/Your_Key/?value1=LG&value2=unoccupied
+  String url = "/trigger/" + Maker_Event + "/with/key/" + Your_Key + "/?value1=" + CRloc + "&value2=" + printState;
   Serial.print("Requesting URL: ");
   Serial.println(url);
-
   // This will send the request to the server
   client.print(String("GET ") + url + " HTTP/1.1\r\n" +
                "Host: " + host + "\r\n" +
                "Connection: close\r\n\r\n");
+  // Read all the lines of the reply from server and print them to Serial
+  while (client.connected()) {
+    if (client.available()) {
+      String line = client.readStringUntil('\r');
+      Serial.print(line);
+    }
+    else {
+      // No data yet, wait a bit
+      delay(50);
+    };
+  }
+  Serial.println();
+  Serial.println("closing connection");
+}
 
+void sendTweet(int State) {
+  if (State == HIGH) {
+    // state = high = no light;
+    printState = "VACANT";
+    Serial.println(printState);     //for debugging
+  }
+  else  {
+    //  state = low = light present;
+    printState = "OCCUPIED";
+    Serial.println(printState);     //for debugging
+  }
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  const int httpPort = 80;
+  while (!client.connect(host, httpPort)) {
+    delay(500);
+    Serial.print(".");
+  }
+// create URL request
+// https://maker.ifttt.com/trigger/IoToilet_tweet/with/key/Your_Key/?value1=LG&value2=unoccupied
+  String url = "/trigger/" + Maker_Event2 + "/with/key/" + Your_Key2 + "/?value1=" + CRloc + "&value2=" + printState;
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+  // This will send the request to the server
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "Connection: close\r\n\r\n");
   // Read all the lines of the reply from server and print them to Serial
   while (client.connected()) {
     if (client.available()) {
